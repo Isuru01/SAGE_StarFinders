56d3bb52dede27bcba430bcd03c55299579d19e2
iamakmal <m.akmalalavi@gmail.com>
2023-08-17 21:26:41 +0530

    mongo uri

--- backend/config/db.config.mjs
+++ backend/config/db.config.mjs
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-<<<<<<< HEAD
   const uri = `mongodb+srv://abcduser:abcd123@clusterrootcode.3rawc27.mongodb.net/`;
-=======
-  const uri = ``;
->>>>>>> c8f33c16733573f1c07aa4b4a8985c33fc84fb56
   mongoose.set("strictQuery", true);
 
   try {
