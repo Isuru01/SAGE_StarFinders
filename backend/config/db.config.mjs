530e160ada20e9c310abdefdb8461b1c700799e8
iamakmal <m.akmalalavi@gmail.com>
2023-08-17 21:25:02 +0530

    cluster uri

--- backend/config/db.config.mjs
+++ backend/config/db.config.mjs
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  const uri = `mongodb+srv://isuruakalanka071:pMIlV5ojDlO69tMx@learning.1yo64vy.mongodb.net/medi_care?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://abcduser:abcd123@clusterrootcode.3rawc27.mongodb.net/`;
   mongoose.set("strictQuery", true);
 
   try {
