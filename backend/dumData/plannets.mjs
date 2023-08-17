1a4d4542af0349c608bdbc20294eafcbeb2ecefb
IT21305900 <isuruakalanka071@gmail.com>
2023-08-17 21:38:37 +0530

    plannet  dummy data

--- backend/dumData/plannets.mjs
+++ backend/dumData/plannets.mjs
@@ -1,1 +1,18 @@
-const plannets = [];
+const plannets = [
+  {
+    pid: "MER",
+    plannet: "Mercury",
+    description: "",
+  },
+  {
+    pid: "VEN",
+    plannet: "Venus",
+    description: "",
+  },
+  {
+    pid: "MAR",
+    plannet: "MARS",
+    description: "",
+  },
+  {},
+];
